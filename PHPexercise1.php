79fd6ec45a4cfa9486cb972cacfdd73c05d7273a
CIT173F-7@M307.UC-BCF.EDU.PH <CIT173F-7@M307.UC-BCF.EDU.PH>
2022-09-15 10:58:33 +0800

    Update folder

---
+++ PHPexercise1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    	<title>PHP exercise 1</title>
+    </head>
+	<body>
+	<?php
+	echo "Twinkle, Twinkle little start! </br>";
+    
+    $twinkle = "Twinkle";
+    $star = "Star";
+    
+    echo " $twinkle, $twinkle little $star! </br> ";
+	?>
+	</body>
+</html>
---
+++ PHPexercise2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    	<title>PHP exercise 2</title>
+    </head>
+	<body>
+	<?php
+	
+		    $x=10;
+	        $y=7;
+	        $sum;
+	       	$difference;
+	        $product;
+	        $quotient;
+	        $remainder;
+	
+
+	        $sum=$x+$y;
+	        $difference=$x-$y;
+	        $product=$x*$y;
+	        $quotient=$x/$y;
+	        $remainder=$x%$y;
+	
+
+	        echo "$x + $y = $sum</br>";
+	        echo "$x - $y = $difference</br>";
+	        echo "$x * $y = $product</br>";
+	        echo "$x / $y = $quotient</br>";
+	        echo "$x % $y = $remainder</br>";
+	        
+	  ?>
+
+</body>
+</html>
